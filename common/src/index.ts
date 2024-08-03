765f74bbe940a62821e0530904ed2410f665d986
Ayush Acharjya <ayushacharjya@gmail.com>
2024-08-04 00:07:17 +0100

    created the export zod types and published as npm package

---
+++ common/src/index.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+
+export const signupInput = z.object({
+  email: z.string().email(),
+  name: z.string().optional(),
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+    .regex(/\d/, "Password must contain at least one number")
+    .regex(/[\W_]/, "Password must contain at least one special character"),
+});
+
+export const signinInput = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export const createBlogInput = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+export const updateBlogInput = z.object({
+  title: z.string(),
+  content: z.string(),
+  id: z.string(),
+});
+
+//type inference
+export type SignupInput = z.infer<typeof signupInput>;
+export type SigninInput = z.infer<typeof signinInput>;
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
