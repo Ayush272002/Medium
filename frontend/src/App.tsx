84d4a4003d8efb7d21da7cb686027d29cafec459
Ayush Acharjya <ayushacharjya@gmail.com>
2024-08-04 00:58:50 +0100

    initialise routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Blog from "./pages/Blog";
+
 function App() {
   return (
     <>
-      <h1 className="bg-red-500">Hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signup" />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
